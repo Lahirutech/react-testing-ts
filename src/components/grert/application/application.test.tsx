e97dc95d961b2243e9b1dcb99a28eb46e0da6af1
lahirutech <rmllbrathnayaka@gmail.com>
2022-09-20 15:15:32 +0530

    adding tests

--- src/components/grert/application/application.test.tsx
+++ src/components/grert/application/application.test.tsx
@@ -4,8 +4,10 @@ import { Application } from "./application"
 describe("Applicationm", () => {
     test("renders correctly", () => {
         render(<Application />)
-        // const nameElement = screen.getByRole("textBox")
-        // expect(nameElement).toBeInTheDocument();
+        const nameElement = screen.getByRole('textbox', {
+            name: 'Name',
+          })
+          expect(nameElement).toBeInTheDocument()
 
         const location = screen.getByRole("combobox")
         expect(location).toBeInTheDocument();
---
+++ src/components/skills/skills.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react"
+import { Skills } from "./skills"
+
+describe('Skills', () => {
+    const skills = ['HTML', 'CSS', 'JavaScript']
+    test('renders correctly', () => {
+        render(<Skills skills={skills} />)
+        const listElement = screen.getByRole('list')
+        expect(listElement).toBeInTheDocument()
+    })
+
+    test('renders a list of skills', () => {
+        render(<Skills skills={skills} />)
+        const listItemElements = screen.getAllByRole('listitem')
+        expect(listItemElements).toHaveLength(skills.length)
+      })
+
+})
